d60828b3ccf8cbb84066f3c4cd3237857ef385c2
Essakimuthu yadav <yadavessakimuthu16@gmail.com>
2024-01-09 08:48:43 +0530

    second day commit

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -2,8 +2,11 @@ package com.smart.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.smart.entity.Contact;
 
 @Controller
 @RequestMapping("/user")
@@ -11,8 +14,17 @@ public class UserController {
 
 	@GetMapping("/index")
 	@PreAuthorize("hasAuthority('USER')")
-	public String dashboard() {
+	public String dashboard(Model model) {
+	
 		return "normal/user_dashboard";
+	}
+	
+	@GetMapping("/add-contact")
+	public String openAddContactForm(Model model) {
+		
+		model.addAttribute("title","Add Contact");
+		model.addAttribute("contact",new Contact());
+		return "normal/add_contact_form";
 	}
 	
 }
